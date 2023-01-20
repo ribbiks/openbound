2a34725ce873e4fbe903cc6690f5c5142e7a4d8a
ribbiks <unconfigured@null.spigotmc.org>
2023-01-19 21:27:39 -0600

    map properties mode is functional

---
+++ source/draggableobject.py
@@ -0,0 +1,41 @@
+import pygame
+
+from pygame.math import Vector2
+
+from source.geometry import point_in_box_excl
+from source.globals  import GRID_SIZE
+
+class DraggableObject:
+	def __init__(self, center_pos, radius):
+		self.center_pos  = center_pos
+		self.radius      = radius
+		self.is_selected = False
+		self.images = []
+
+	def add_image(self, img_fn):
+		self.images.append(pygame.image.load(img_fn).convert_alpha())
+
+	def update(self, mousepos, grab_action, release_action):
+		tl = Vector2(self.center_pos.x - self.radius, self.center_pos.y - self.radius)
+		br = Vector2(self.center_pos.x + self.radius, self.center_pos.y + self.radius)
+		released = False
+		if grab_action:
+			if point_in_box_excl(mousepos, tl, br):
+				self.is_selected = True
+			else:
+				self.is_selected = False
+				released = True
+		if release_action:
+			self.is_selected = False
+			released = True
+		#
+		if self.is_selected:
+			snap_x = int(mousepos.x/GRID_SIZE + 0.5) * GRID_SIZE
+			snap_y = int(mousepos.y/GRID_SIZE + 0.5) * GRID_SIZE
+			self.center_pos = Vector2(snap_x, snap_y)
+		return released
+
+	def draw(self, screen, offset):
+		for img in self.images:
+			new_rect = img.get_rect(center=img.get_rect(center=self.center_pos+offset).center)
+			screen.blit(img, new_rect)
